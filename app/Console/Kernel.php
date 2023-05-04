616ccfbba1dd8afa4f13500d57dc478f8438821f
Sejati Adli <sejatiadli@gmail.com>
2023-05-04 12:51:19 +0700

    [SA] make scheduler for every midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new NotifEventJob())->everyMinute();
+        $schedule->job(new NotifEventJob())->daily();
     }
 
     /**
