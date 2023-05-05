9f7be33571017dc20fd72fd429cca0a4e0adc28a
Sejati Adli <sejatiadli@gmail.com>
2023-05-05 09:47:55 +0700

    [SA] make scheduler for every day at 7pm

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new NotifEventJob())->daily();
+        $schedule->job(new NotifEventJob())->dailyAt('07:00');
     }
 
     /**
