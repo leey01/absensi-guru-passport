476b8883583fa4340d84e87f9c8910694f9ef9d9
Sejati Adli <sejatiadli@gmail.com>
2023-06-09 06:56:43 +0700

    [SA] fix/benerin logic absen masuk dan pulang, bikin query untuk get data kehadiran di profile

---
+++ database/migrations/2023_06_08_120144_add_column_valid_to_absensis_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('absensis', function (Blueprint $table) {
+            $table->boolean('valid_masuk')->default(false)->after('keterangan');
+            $table->boolean('valid_pulang')->default(false)->after('longitude_masuk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('absensis', function (Blueprint $table) {
+        });
+    }
+};
