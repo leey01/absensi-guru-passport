dca5c3f2068a79646e1dad28ae31a25f6a52da93
Sejati Adli <sejatiadli@gmail.com>
2023-06-19 07:07:53 +0700

    [SA] ganti jadwal notif

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new NotifEventJob())->dailyAt('07:00');
+        $schedule->job(new NotifEventJob())->dailyAt('07:15');
         $schedule->job(new AbsenJob())->dailyAt('23:00');
     }
 
