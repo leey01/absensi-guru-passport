3e3a636e899eac1903f03e4771ba7213fd836bef
Sejati Adli <sejatiadli@gmail.com>
2023-08-29 11:44:51 +0700

    [SA] bikin fitur create update di import user

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -4,26 +4,32 @@ namespace App\Imports;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Collection;
 
-class UsersImport implements ToModel, WithHeadingRow
+class UsersImport implements ToCollection, WithHeadingRow
 {
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
-    public function model(array $row)
+    public function collection(Collection $rows)
     {
-        return new User([
-            'niy'      => $row['niy'],
-            'nama'     => $row['nama'],
-            'email'    => $row['email'],
-            'password' => Hash::make($row['password']),
-            'alamat'   => $row['alamat'],
-            'no_hp'    => $row['no_hp'],
-        ]);
+        foreach ($rows as $row) {
+            $user = User::updateOrCreate(
+                ['niy' => $row['niy']], // Kolom unik untuk pengecekan
+                [
+                    'nama'     => $row['nama'],
+                    'email'    => $row['email'],
+                    'password' => Hash::make($row['password']),
+                    'alamat'   => $row['alamat'],
+                    'no_hp'    => $row['no_hp'],
+                ]
+            );
+        }
     }
 
     public function headingRow(): int
